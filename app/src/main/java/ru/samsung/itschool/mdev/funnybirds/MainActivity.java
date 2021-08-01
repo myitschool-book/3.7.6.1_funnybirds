1d0258f24a59fd8c0aa1f41492789963a5e81bdf
Pavel <impetus@yandex.ru>
2021-08-01 18:51:01 +0300

    first commit

---
+++ app/src/main/java/ru/samsung/itschool/mdev/funnybirds/MainActivity.java
@@ -0,0 +1,16 @@
+package ru.samsung.itschool.mdev.funnybirds;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        setContentView(new GameView(this));
+    }
+
+}
